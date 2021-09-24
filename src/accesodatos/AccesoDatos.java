ec43b8a9de7f0fd88ed967dad7167d8e4232bffa
Marco <marcoijsl@gmail.com>
2021-09-24 17:51:31 +0200

    Creación del proyecto

---
+++ src/accesodatos/AccesoDatos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package accesodatos;
+
+/**
+ *
+ * @author Alumno Tarde
+ */
+public class AccesoDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
